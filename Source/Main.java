e7a02a29e008dc4a805b1dc372f797d77a312fc8
이동준 <bhdj0107@gmail.com>
2020-06-29 15:00:55 +0900

    Socket 수정

--- Source/Main.java
+++ Source/Main.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 import java.net.*;
 public class Main extends JFrame {
 	Socket sock;
+	boolean accepted = false;
+	// jf1 상대의 메시지, jf2 나의 입력
 	JTextField jf1 = new JTextField();
 	JTextField jf2 = new JTextField();
 	DataOutputStream osDataStream = null;
@@ -16,31 +18,47 @@ public class Main extends JFrame {
 		JPanel pn = new JPanel();
 		setContentPane(pn);
 		pn.setLayout(null);
-		
-		// UI 배치
+
+		// Mode 설정
 		JButton server = new JButton("서버 열기");
+		JLabel ip = new JLabel("IP주소 : ");
+		JTextField ipAddress = new JTextField();
+		JButton ipButton = new JButton("연결");
+		JLabel Mode = new JLabel();
+		Mode.setBounds(15,5,400,20);
+		JPanel modeset = new JPanel();
+		modeset.setBounds(0,5,430,20);
+		modeset.setVisible(false);
+		
+		modeset.add(Mode);
+		pn.add(modeset);
+		
 		server.setBounds(20,5,100,20);
 		server.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
+					// 프로그램 Title 변경 후, Server 메소드 실행
 					InetAddress Address = InetAddress.getLocalHost();
 					setTitle("메신저 프로그램 / 서버 Mode / " + Address.toString().split("/")[1]);
 					server();
+					server.setVisible(false);
+					ip.setVisible(false);
+					ipAddress.setVisible(false);
+					ipButton.setVisible(false);
+					Mode.setText("Server 모드로 설정되었습니다.");
+					modeset.setVisible(true);
+					
 				} catch (UnknownHostException e1) {
 					e1.printStackTrace();
 				}
 			}
 		});
 		pn.add(server);
-		JLabel ip = new JLabel("IP주소 : ");
 		ip.setBounds(190,5, 100, 20);
 		pn.add(ip);
-		
-		JTextField ipAddress = new JTextField();
 		ipAddress.setBounds(240,5, 100, 20);
 		pn.add(ipAddress);
 		
-		JButton ipButton = new JButton("연결");
 		ipButton.setBounds(350,5,60,20);
 		ipButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -80,35 +98,12 @@ public class Main extends JFrame {
 		
 	}
 	public void server() {
-		ServerSocket serverSock = null;
-		try {
-			InetAddress serverAddr = InetAddress.getByName(null);
-			serverSock = new ServerSocket(5095, 1);
-			sock = serverSock.accept();
-			jf1.setText("연결됨");
-			osDataStream = new DataOutputStream(sock.getOutputStream());
-			new ReceiveDataThread(this).start();
-		} catch (IOException e) {		} finally {
-			if (serverSock != null) {
-				try {
-					serverSock.close();
-				} catch (IOException x) {
-				}
-			}
-		}
+		new ServerSockThread(this).start();
+		new ReceiveDataThread(this).start();
 	}
 	public void client(String serverName) {
-		try {
-			if (serverName.equals("localhost")) 
-				serverName = null;
-			InetAddress serverAddr = InetAddress.getByName(serverName);
-			sock = new Socket(serverAddr.getHostName(), 5095);
-			jf1.setText("연결됨");
-			osDataStream = new DataOutputStream(sock.getOutputStream());
-			new ReceiveDataThread(this).start();
-		} catch (IOException e) {
-			
-		}
+		new ClientSockThread(this, serverName).start();
+		new ReceiveDataThread(this).start();
 	}
 	class TextActionHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -139,6 +134,58 @@ public class Main extends JFrame {
 		new Main();
 	}
 }
+class ServerSockThread extends Thread {
+	private Main chat;
+	private ServerSocket serverSock = null;
+	private Socket sock = null;
+	ServerSockThread(Main chat) {
+		this.chat = chat;
+	}
+	public void run() {
+		try {
+			InetAddress serverAddr = InetAddress.getByName(null);
+			serverSock = new ServerSocket(5095, 1);
+			chat.sock = serverSock.accept();
+			chat.jf1.setText("연결됨");
+			chat.osDataStream = new DataOutputStream(chat.sock.getOutputStream());
+			chat.accepted = true;
+		} catch (IOException e) {
+		} finally {
+			if (serverSock != null) {
+				try {
+					serverSock.close();
+				} catch (IOException x) {
+				}
+			}
+		}
+	}
+}
+class ClientSockThread extends Thread {
+	private Main chat;
+	private ServerSocket serverSock = null;
+	private Socket sock = null;
+	private String serverName;
+	ClientSockThread(Main chat, String serverName) {
+		this.chat = chat;
+		this.serverName = serverName;
+	}
+	public void run() {
+		try {
+			if (serverName.equals("localhost")) 
+				serverName = null;
+			InetAddress serverAddr = InetAddress.getByName(serverName);
+			chat.sock = new Socket(serverAddr.getHostName(), 5095);
+			chat.jf1.setText("연결됨");
+			chat.osDataStream = new DataOutputStream(chat.sock.getOutputStream());
+			chat.accepted = true;
+		} catch (IOException e) {
+			
+		}
+	}
+	public Socket sock() {
+		return sock;
+	}
+}
 class ReceiveDataThread extends Thread {
 	private Main chat;
 	private DataInputStream isDataStream;
@@ -148,6 +195,14 @@ class ReceiveDataThread extends Thread {
 	}
 	public synchronized void run() {
 		String str;
+		while(true) {
+			if(chat.accepted) break;
+			try {
+				sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		try {
 			isDataStream = new DataInputStream(chat.sock.getInputStream());
 			while (bWaitting) {
